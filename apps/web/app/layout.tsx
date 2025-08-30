0d0c994edca8a014624a099d487db85e9f871f11
Saheed Odulaja <saheedodulaja@gmail.com>
2025-08-30 21:38:18 +0100

    Implement React Bootstrap

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import "bootstrap/dist/css/bootstrap.min.css"; // Add this line at the top
 import "./globals.css";
 
 const geistSans = localFont({
